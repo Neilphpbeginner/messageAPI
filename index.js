f5f660dd8aebe8749535ec331da0e156b10ad228
Neil Lemmer <lemmer.neil@gmail.com>
2020-04-26 08:59:40 +0200

    Fixed index.js file

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.use('/api', router)
 
 // Starting Server
 
-app.listen(3000, () => {
-    console.log("Server up and running")
-})
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
